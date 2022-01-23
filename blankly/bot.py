9aad9b44598c1d49702d28fac66883a9e2b57ee5
Andrew <dragonpulse321@gmail.com>
2022-01-22 21:34:50 -0500

    splitting data part 1

--- blankly/bot.py
+++ blankly/bot.py
@@ -1,4 +1,9 @@
 import blankly
+from blankly.indicators import rsi, macd, sma
+from blankly.utils import trunc
+# API SECRET KEY: eZUlyxbZ1AP+ZN92QJJ+MCwc325NUMxCc8CZHpVN/Fj8oX+yXgOCoDwv6BEegYUq9f/x6vaXV0ycxfOiJ+tPgg==
+# passphrase: jkaz04251211
+# API KEY: 4cedb7e759bc6cc696933be5375fdba4
 
 
 def price_event(price, symbol, state: blankly.StrategyState):
@@ -7,27 +12,39 @@ def price_event(price, symbol, state: blankly.StrategyState):
     rsi = blankly.indicators.rsi(state.variables['history'])
     if rsi[-1] < 30 and not state.variables['owns_position']:
         # Dollar cost average buy
-        buy = int(state.interface.cash/price)
+        buy = trunc(state.interface.cash/price,8)
         state.interface.market_order(symbol, side='buy', size=buy)
         state.variables['owns_position'] = True
     elif rsi[-1] > 70 and state.variables['owns_position']:
         # Dollar cost average sell
-        curr_value = int(state.interface.account[state.base_asset].available)
+        curr_value = state.interface.account[state.base_asset].available
         state.interface.market_order(symbol, side='sell', size=curr_value)
         state.variables['owns_position'] = False
 
 
 def init(symbol, state: blankly.StrategyState):
     # Download price data to give context to the algo
-    state.variables['history'] = state.interface.history(symbol, to=150, return_as='deque',
+    state.variables['open_history'] = state.interface.history(symbol, to=150, return_as='deque',
+                                                         resolution=state.resolution)['open']
+    state.variables['high_history'] = state.interface.history(symbol, to=150, return_as='deque',
+                                                         resolution=state.resolution)['high']
+    state.variables['low_history'] = state.interface.history(symbol, to=150, return_as='deque',
+                                                         resolution=state.resolution)['low']                                                     
+    state.variables['close_history'] = state.interface.history(symbol, to=150, return_as='deque',
                                                          resolution=state.resolution)['close']
+    state.variables['volume_history'] = state.interface.history(symbol, to=150, return_as='deque',
+                                                         resolution=state.resolution)['volume']
+    
     state.variables['owns_position'] = False
 
 
 if __name__ == "__main__":
+    # Data preprocessing
+
+
     # Authenticate coinbase pro strategy
     exchange = blankly.CoinbasePro(portfolio_name="my cool portfolio")
-    paper_trade = blankly.PaperTrade(exchange, initial_account_values={'USD': 1000})
+    paper_trade = blankly.PaperTrade(exchange, initial_account_values={'USD': 10000})
 
     # Use our strategy helper on coinbase pro
     strategy = blankly.Strategy(paper_trade)
@@ -38,5 +55,5 @@ if __name__ == "__main__":
     # Start the strategy. This will begin each of the price event ticks
     # strategy.start()
     # Or backtest using this
-    results = strategy.backtest(to='1y', initial_values={'USD': 1000})
+    results = strategy.backtest(to='1y', initial_values={'USD': 10000})
     print(results)
---
+++ blankly/bot2.py
@@ -0,0 +1,50 @@
+"""
+Binance Crypto Trading Bot Based on RSI Crossover
+
+This creates a crypto trading bot that buys BTC and ETH when RSI is less than 30 and sells
+when RSI is greater than 70. Easily backtest and take this live by changing a few
+lines of code.
+
+For more information on the blankly package, check out https://docs.blankly.finance
+
+Happy Building :D 
+
+NOTE: Make sure you properly set your binance_tld in the `settings.json`
+NOTE: Set use_sandbox = False since you will want to use Binance Live Data
+"""
+
+from blankly import CoinbasePro, Strategy, StrategyState
+from blankly.utils import trunc
+from blankly.indicators import rsi
+
+def init(symbol, state: StrategyState):
+    state.variables['history'] = state.interface.history(symbol, to=150, 
+        resolution=state.resolution, 
+        return_as='deque')['close']
+    state.variables['owns_position'] = False
+
+def rsi_price_event(price, symbol, state: StrategyState):
+    state.variables['history'].append(price) # appends to the deque of historical prices
+    rsi_output = rsi(state.variables['history'])
+
+    if rsi_output[-1] < 30 and not state.variables['owns_position']:
+        qty = trunc(state.interface.cash * 0.5 / price, 2)
+        state.interface.market_order(symbol, side='buy', size=qty)
+        state.variables['owns_position'] = True
+    elif rsi_output[-1] > 70 and state.variables['owns_position']:
+        # get the amount of bitcoin we have in our account
+        curr_value = trunc(state.interface.account[state.base_asset].available, 2)
+        state.interface.market_order(symbol, side='sell', size=curr_value)
+        state.variables['owns_position'] = False
+    
+exchange = CoinbasePro(portfolio_name="my cool portfolio")
+strategy = Strategy(exchange)
+
+strategy.add_price_event(rsi_price_event, symbol='BTC-USD', resolution='1h', init=init)
+strategy.add_price_event(rsi_price_event, symbol='ETH-USD', resolution='1h', init=init)
+
+results = strategy.backtest(to='2y', initial_values={'USD': 10000})
+print(results)
+
+# Take this strategy live by uncommenting this line
+# strategy.start()
